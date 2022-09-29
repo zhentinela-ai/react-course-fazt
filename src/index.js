8da295f4f1ab6ba058ff7b0ad096d4297b649558
Zhent <jdcaballeror@correo.udistrital.edu.co>
2022-09-29 00:38:13 -0500

    first session

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import react from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function Greeting() {
+  function add(x, y) {
+    return x + y;
+  }
+
+  return (
+    <>
+      <h1>{add(10, 30)}</h1>
+    </>
+  );
+  // return <h1>{married ? "I am married 😀" : "I am not married 😈"}</h1>;
+}
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <Greeting />
+  </>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
